1b1e5bf6afd6d1cc34916a6243b042bb055bd40e
zi78494umbcedu <125627136+zi78494umbcedu@users.noreply.github.com>
2024-04-22 21:22:45 -0400

    Time: 0 ms (100.00%) | Memory: 45.8 MB (21.21%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        //recursive approach
+        return searchRecur(nums, 0, nums.length-1, target);
+        
+    }
+    static int searchRecur(int[] nums, int start, int end, int target){
+        int mid = start+((end-start)/2);
+        if(start>end)return -1;
+        if(nums[mid]==target) return mid;
+        else if(target<nums[mid]){
+            return searchRecur(nums, start, mid-1, target);
+        }
+        return searchRecur(nums, mid+1, end, target);
+    }
+
+
+        
+        //iterative approach
+    //     int start= 0;
+    //     int len = nums.length;
+    //     int end = len-1;
+    //     while(start<=end){
+    //         int mid = (start+end)/2;
+    //         if(nums[mid]==target)return mid;
+    //         else if(nums[mid]<target){
+    //             start = mid+1;
+    //         }else if(nums[mid]>target){
+    //             end = mid-1;
+    //         }
+    //     }
+    // return -1;
+}
