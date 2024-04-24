33b433bc5eee511983f484a20b932356293fe0cc
zi78494umbcedu <125627136+zi78494umbcedu@users.noreply.github.com>
2024-04-23 23:50:09 -0400

    Time: 19 ms (69.68%) | Memory: 57.7 MB (70.25%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+        if(bloomDay.length<(m*k))return -1;
+        int high = getMax(bloomDay);
+        int low = 1;
+        int ans = -1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            int possibleBouquets = getBouquetsMade(bloomDay, k, mid);
+            if(possibleBouquets>=m){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+    static int getBouquetsMade(int[] bloomDay, int k, int days){
+        int bouquets = 0;
+        int consecCount = 0;
+        for(int i=0;i<bloomDay.length;i++){
+            if(bloomDay[i]<=days){
+                consecCount++;
+                if(consecCount==k){
+                    bouquets++;
+                    consecCount=0;
+                }
+            }
+            else{
+                consecCount=0;
+            }
+            if(consecCount==k)bouquets++;
+        }
+        return bouquets;
+    }
+
+    static int getMax(int[] bloomDay){
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<bloomDay.length;i++){
+            max = max>bloomDay[i]?max:bloomDay[i];
+        }
+        return max;
+    }
+}
