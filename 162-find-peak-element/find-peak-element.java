d58bcfeb448babfa7e9c43b02165baf378b41eec
zi78494umbcedu <125627136+zi78494umbcedu@users.noreply.github.com>
2024-04-25 21:40:59 -0400

    Time: 0 ms (100.00%) | Memory: 42.5 MB (17.84%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -10,7 +10,8 @@ class Solution {
             int mid = (low+high)/2;
             if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1])return mid;
             else if(nums[mid]>nums[mid-1])low = mid+1;
-            else high=mid-1;
+            else if(nums[mid]>nums[mid+1])high=mid-1;
+            else low=mid+1;
         }
         return -1;
         //brute force
