524a789226dd7e2bb21b7e7e438502e8ce060fad
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-05-07 02:36:10 -0400

    Time: 0 ms (100.00%) | Memory: 42.2 MB (41.51%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,25 +1,25 @@
 class Solution {
     public double myPow(double x, int n) {
-        if (n == 0) return 1.0;
-        if (x == 0) return 0.0;
-        
-        boolean neg = n < 0;
-        n = Math.abs(n);
-        
-        double result = power(x, n);
-        
-        return neg ? 1 / result : result;
+        //binomial exponential approach
+        if(n==0)return 1;
+        if(x==0)return 0;
+        boolean neg=n<0?true:false;
+        n=Math.abs(n);
+        double pow = power(x, n);
+        return neg?1/pow:pow;
+        //brute force
+        // if(n==0 ||x==0)return 1;
+        // if(n<0)return myPow(x, -n);
+        // return x*myPow(x, n-1);
     }
-    
-    private double power(double x, int n) {
-        if (n == 0) return 1.0;
-        
-        double half = power(x, n / 2);
-        
-        if (n % 2 == 0) {
-            return half * half;
-        } else {
-            return x * half * half;
+
+    static double power(double x, int n){
+        if(n==0)return 1;
+        double half = power(x, n/2);
+        if(n%2==0){
+            return half*half;
+        }else{
+            return x*half*half;
         }
     }
 }
