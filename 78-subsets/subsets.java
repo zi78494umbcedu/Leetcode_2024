824abad0770d24738f18d1e4fe37d11e22beceb1
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-05-14 12:26:45 -0400

    Time: 0 ms (100.00%) | Memory: 42.6 MB (59.81%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,52 +1,21 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        //recursion
-        //initilialising the main returnable list of lists/subsets
+        //bit manipulation method
+        //looping across the number of subsets => 2^-1
+        int n = nums.length;
         List<List<Integer>> subsets = new ArrayList<>();
-        int n  = nums.length-1;
-        //calling the recursive function
-        recurSubsets(subsets, nums, 0, new ArrayList<>());
+        for(int i=0;i<(1<<n);i++){
+            //initialising internal subset space
+            List<Integer> sub = new ArrayList<>();
+            //looping through the length of the Array
+            for(int j=0;j<n;j++){
+                //checking if the bit is set for populating the integer in the place
+                if((i&(1<<j))!=0){
+                    sub.add(nums[j]);
+                }
+            }
+            subsets.add(sub);
+        } 
         return subsets;
     }
-        //bit manipulation method
-        // //list of list of integers
-        // List<List<Integer>> subs = new ArrayList<>();
-        // //iterate over 2^n-1
-        // for(int i=0;i<=Math.pow(2, nums.length)-1;i++){
-        //     //to store current subset generated
-        //     List<Integer> tub = new ArrayList<>();
-        //     //checking if jth bit of i is set
-        //     for(int j=0;j<=nums.length-1;j++){
-        //         if((i&(1<<j))!=0){
-        //             tub.add(nums[j]);
-        //         }
-        //     }
-        //     subs.add(tub);
-        // } 
-        // return subs;
-
-    static void recurSubsets(List<List<Integer>> subsets, int[] nums, int i, List<Integer> tempList){
-        if(i==nums.length){
-            subsets.add(new ArrayList<>(tempList));
-            return;
-        }
-        tempList.add(nums[i]);
-        recurSubsets(subsets, nums, i+1, tempList);
-        tempList.remove(tempList.size()-1);
-        recurSubsets(subsets, nums, i+1, tempList);
-    }
 }
-
-/**
-2^n-1
-
-0 0 0 
-0 0 1
-0 1 0 
-0 1 1
-1 0 0
-1 0 1
-1 1 0 
-1 1 1 
-
- */
