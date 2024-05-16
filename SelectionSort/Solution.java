dfebb8d033c1432cd8dd1f2d1198deef2e4ab450
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-05-16 08:43:29 -0400

    Create Solution.java

---
+++ SelectionSort/Solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        //iterating over the loop for minumum from 1st number to n-2, 
+        //since last number trivial to compare for minimum
+        for(int i=0;i<=n-2;i++){
+            //having 1st element of loop as minimum for having lowest of the subarray over here
+            int min = i;
+            //iterating over subarray
+            for(int j=i;j<=n-1;j++){
+                //if min found
+                if(arr[min]>arr[j]){
+                    //replace index for further swapping with the first element
+                    min= j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+}
