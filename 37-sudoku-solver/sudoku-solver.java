fc9d8cfde4ab301611400b59fa016e981b0489da
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-05-18 12:04:50 -0400

    Time: 11 ms (36.10%) | Memory: 41.2 MB (22.99%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+    static boolean isValid(int row, int col, char c, char[][] board){
+        for(int i=0;i<9;i++){
+            if(board[row][i]==c){
+                return false;
+            }
+            if(board[i][col]==c){
+                return false;
+            }
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static boolean solve(char[][] board){
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
+                if(board[i][j]=='.'){
+                    for(char c='1';c<='9';c++){
+                        if(isValid(i, j, c, board)){
+                            board[i][j]=c;
+                            if(solve(board)==true){
+                                return true;
+                            }
+                            else{
+                                board[i][j]='.';
+                            }
+                        }
+                        
+                    } 
+                    return false;  
+                }
+            }
+        }
+        return true;
+    }
+}
