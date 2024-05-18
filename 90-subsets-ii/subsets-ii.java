dc7a71ea794cd7dfb5ff2c344c16ab9c3efa7be0
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-05-17 23:51:12 -0400

    Time: 1 ms (99.83%) | Memory: 43.4 MB (39.39%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,10 +1,12 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> powerSet = new ArrayList<>();
+        //to make the duplicates together since our logic of looping through the array is dependent
         Arrays.sort(nums);
         recurCallToPower(0, nums, powerSet, new ArrayList<>());
         return powerSet;
     }
+    //reducing time from 2^n*k*logn by some factor by looping over the array to avoid duplicate arrays which the sort and hashset is helping remove but adding complexty
     static void recurCallToPower(int ind, int[] nums, List<List<Integer>> subsets, List<Integer> tempList){
         subsets.add(new ArrayList<>(tempList));
         for(int i=ind;i<nums.length;i++){
