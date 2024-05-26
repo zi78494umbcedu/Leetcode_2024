253066348d75bc2714281bf095d43400b5e2ec39
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-05-26 14:13:59 -0400

    Time: 0 ms (100.00%) | Memory: 41.1 MB (56.29%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,13 +1,11 @@
 class Solution {
     public boolean check(int[] nums) {
         int count=0;
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]>nums[i+1]){
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>nums[(i+1)%nums.length]){
                 count++;
             }
         }
-        //edge case indicating inversion
-        if(nums[0]<nums[nums.length-1])count++;
         return count<=1;
     }
 }
