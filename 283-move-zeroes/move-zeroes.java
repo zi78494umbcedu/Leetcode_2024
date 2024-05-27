a8a4daf270b031566ba46f617b02cc6b4194baa7
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-05-27 11:39:58 -0400

    Time: 1 ms (100.00%) | Memory: 45.7 MB (85.57%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,14 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        if(nums.length==1)return;
+        if(nums.length==1 || nums.length==0)return;
         int nonZeroIndex = 0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
                 nums[nonZeroIndex] = nums[i];
+                //makes index +1 in the final case
                 nonZeroIndex++;
             }
         }
         while(nonZeroIndex<nums.length){
+            //zero can be placed non Zero Index directly
             nums[nonZeroIndex] =0;
             nonZeroIndex++;
         }
