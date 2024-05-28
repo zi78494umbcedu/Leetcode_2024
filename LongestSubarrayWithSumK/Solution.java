06e4393996c8d2cfff30b1dd1ae4e95d70f3dbe3
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-05-27 20:46:08 -0400

    Update Solution.java

--- LongestSubarrayWithSumK/Solution.java
+++ LongestSubarrayWithSumK/Solution.java
@@ -3,30 +3,51 @@ import java.util.*;
 
 public class Solution {
     public static int longestSubarrayWithSumK(int []a, long k) {
-        //hashing
-        HashMap<Long, Integer> map = new HashMap<>();
-        int len = 0;
-        int maxLen=0;
-        long sum=0;
-        for(int i=0;i<a.length;i++){
-            sum+=a[i];
-            if(sum==k){
-                maxLen = Math.max(maxLen, i+1);
-            }
-            //remaining sum
-            long remainingSum = sum-k;
-            //find the new len and update maxLen
-            if(map.containsKey(remainingSum)){
-                //forward length of the new subarray found
-                len = i-map.get(remainingSum);
-                maxLen = Math.max(len, maxLen);
-            }
-            //updating map with sum 
-            if(!map.containsKey(sum)){
-                map.put(sum, i);
-            }
+        //2-pointer approach
+        int maxLen = 0;
+        long sum=a[0];
+       int left=0;
+       int right =0;
+       int n=a.length;
+       while(right<n){
+           //remove elements from left 
+           while(left<=right && sum>k){
+               sum-=a[left];
+               left++;
+           }
+           //found subarray of sum k
+           if(sum==k)maxLen = Math.max(maxLen, right-left+1);
+           //adding to sum while careful about the edge
+           right++;
+           if(right<n){
+               sum+=a[right];
+           }
         }
         return maxLen;
+        //hashing
+        // HashMap<Long, Integer> map = new HashMap<>();
+        // int len = 0;
+        // int maxLen=0;
+        // long sum=0;
+        // for(int i=0;i<a.length;i++){
+        //     sum+=a[i];
+        //     if(sum==k){
+        //         maxLen = Math.max(maxLen, i+1);
+        //     }
+        //     //remaining sum
+        //     long remainingSum = sum-k;
+        //     //find the new len and update maxLen
+        //     if(map.containsKey(remainingSum)){
+        //         //forward length of the new subarray found
+        //         len = i-map.get(remainingSum);
+        //         maxLen = Math.max(len, maxLen);
+        //     }
+        //     //updating map with sum 
+        //     if(!map.containsKey(sum)){
+        //         map.put(sum, i);
+        //     }
+        // }
+        // return maxLen;
         //approach 2 O(n^2)
         // int len=0;
         // for(int i=0;i<a.length;i++){
