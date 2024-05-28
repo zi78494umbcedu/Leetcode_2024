abc2f46b517b0922f16cfd18b9ee7de4184571d0
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-05-28 18:04:07 -0400

    Time: 1 ms (84.53%) | Memory: 41.7 MB (68.07%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,18 +1,18 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        //brute force using (R-1)C(N-1)
+        // //brute force using (R-1)C(N-1)
         List<List<Integer>> pascals = new ArrayList<>();
-        for(int i=0;i<numRows;i++){
+        for(int row=0;row<numRows;row++){
             List<Integer> pascalRow= new ArrayList<>();
-            for(int j=0;j<=i;j++){
-                pascalRow.add(calculateRCN(j, i));
+            for(int col=0;col<=row;col++){
+                pascalRow.add(calculateRCN(row, col));
             }
             pascals.add(pascalRow);
         }
         return pascals;
 
     }
-    static int calculateRCN(int r, int n){
+    static int calculateRCN(int n, int r){
         if(r==0 || n==r)return 1;
         //10C2 = 10*9/1*2
         int res=1;
