bfacaef1e94eb3ea258c4c85d70e38dabec16091
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-05-27 22:32:39 -0400

    Time: 2 ms (97.67%) | Memory: 45 MB (13.89%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,17 +1,25 @@
-import java.util.HashMap;
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] ind = new int[2];
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for(int i=0;i<nums.length;i++){
+        //optimal approach
+        //using 2 pointer after sorting, add to left and subtract from right if sum is less and more respectively
+        //better approach hashing O(nLogn)
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        for(int i=0;i<n;i++){
             if(map.containsKey(target-nums[i])){
-                    ind[1]=i;
-                    ind[0]=map.get(target-nums[i]);
-                    return ind;
+                return new int[]{i, map.get(target-nums[i])};
             }
             map.put(nums[i], i);
         }
-        return ind;
+        return new int[]{};
+        //brute force O(n^2)
+        // for(int i=0;i<nums.length;i++){
+        //      for(int j=i+1;j<nums.length;j++){
+        //         if(nums[j]+nums[i]==target){
+        //             return new int[]{i, j};
+        //         }
+        //      }
+        // }
+        // return new int[]{};
     }
 }
