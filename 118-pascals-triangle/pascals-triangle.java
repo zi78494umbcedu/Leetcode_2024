ae1f0789b2c408b3d2e20a5ec007515197146130
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-05-28 18:16:28 -0400

    Time: 1 ms (84.53%) | Memory: 41.8 MB (51.16%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,16 +1,35 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        // //brute force using (R-1)C(N-1)
+        //better approach
         List<List<Integer>> pascals = new ArrayList<>();
-        for(int row=0;row<numRows;row++){
-            List<Integer> pascalRow= new ArrayList<>();
-            for(int col=0;col<=row;col++){
-                pascalRow.add(calculateRCN(row, col));
-            }
-            pascals.add(pascalRow);
+        for(int rows = 0;rows<numRows;rows++){
+            List<Integer> pascalRow=new ArrayList<>();
+            pascals.add(generateRow(pascalRow, rows));
         }
         return pascals;
+        // // //brute force using (R-1)C(N-1)
+        // List<List<Integer>> pascals = new ArrayList<>();
+        // for(int row=0;row<numRows;row++){
+        //     List<Integer> pascalRow= new ArrayList<>();
+        //     for(int col=0;col<=row;col++){
+        //         pascalRow.add(calculateRCN(row, col));
+        //     }
+        //     pascals.add(pascalRow);
+        // }
+        // return pascals;
 
+    }
+    static List<Integer> generateRow(List<Integer> rowList, int rowNumber){
+            int ans=1;
+            rowList.add(1);
+            for(int col=0;col<rowNumber;col++){
+                ans = ans * (rowNumber-col);
+                ans = ans / (col+1);
+                //System.out.println(rowNumber+":"+col+":"+ans);
+                rowList.add(ans);
+            }
+            return rowList;
+        
     }
     static int calculateRCN(int n, int r){
         if(r==0 || n==r)return 1;
