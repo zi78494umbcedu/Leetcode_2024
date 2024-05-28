d26df728b1007a69656b80d74470c49d5d184e26
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-05-28 08:04:21 -0400

    Time: 3 ms (100.00%) | Memory: 76.8 MB (68.06%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,20 +1,36 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int n= nums.length;
-         List<Integer> pos = new ArrayList<>(n/2);
-        List<Integer> neg = new ArrayList<>(n/2);
+        //better approach with one pass
+        int[] answer = new int[nums.length];
+        int posIndex=0;
+        int negIndex=1;
+        int n=nums.length;
         for(int i=0;i<n;i++){
             if(nums[i]<0){
-                neg.add(nums[i]);
+                answer[negIndex] = nums[i];
+                negIndex+=2;
             }else{
-                pos.add(nums[i]);
+                answer[posIndex] = nums[i];
+                posIndex+=2;
             }
         }
-        for(int i=0;i<n/2;i++){
-            nums[2*i] = pos.get(i);
-            nums[2*i+1] = neg.get(i);
-        }
-        return nums;
+        return answer;
+        //brute force O(2*N) + O(N)
+        // int n= nums.length;
+        // List<Integer> pos = new ArrayList<>(n/2);
+        // List<Integer> neg = new ArrayList<>(n/2);
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]<0){
+        //         neg.add(nums[i]);
+        //     }else{
+        //         pos.add(nums[i]);
+        //     }
+        // }
+        // for(int i=0;i<n/2;i++){
+        //     nums[2*i] = pos.get(i);
+        //     nums[2*i+1] = neg.get(i);
+        // }
+        // return nums;
     }
 }
 
