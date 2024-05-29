96514e9361f80b6740d6ebbbcb3651ee6caa9fc3
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-05-29 18:18:14 -0400

    Time: 7 ms (96.47%) | Memory: 46.5 MB (32.34%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,31 +1,48 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        //step1 sorting -> step2 comparing 1.2, 2.1 and merge
-        //sorting the array
-        Arrays.sort(intervals, new Comparator<int[]>(){
-            public int compare(int[] a,int[] b){
+        //in one pass O(n)
+        //step1 sorting
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b){
                 return a[0]-b[0];
             }
         });
         int n = intervals.length;
         List<int[]> response = new ArrayList<>();
         for(int i=0;i<n;i++){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            //skipping the elements through the array already scanned
-            if(!response.isEmpty() && end<=response.get(response.size()-1)[1]){
-                continue;
+            if(response.isEmpty() || intervals[i][0]>response.get(response.size()-1)[1]){
+                response.add(intervals[i]);
+            }else{
+                response.get(response.size()-1)[1] = Math.max(response.get(response.size()-1)[1],intervals[i][1]);
             }
-            for(int j=i+1;j<n;j++){
-                if(intervals[j][0]<=end){
-                    end = Math.max(end, intervals[j][1]);
-                }else{
-                    break;
-                }
-            }
-            response.add(new int[]{start, end});
         }
-        return response.toArray(new int[response.size()][]);
+        return response.toArray(new int[response.size()-1][]);
+        // //step1 sorting -> step2 comparing 1.2, 2.1 and merge
+        // //sorting the array
+        // Arrays.sort(intervals, new Comparator<int[]>(){
+        //     public int compare(int[] a,int[] b){
+        //         return a[0]-b[0];
+        //     }
+        // });
+        // int n = intervals.length;
+        // List<int[]> response = new ArrayList<>();
+        // for(int i=0;i<n;i++){
+        //     int start = intervals[i][0];
+        //     int end = intervals[i][1];
+        //     //skipping the elements through the array already scanned
+        //     if(!response.isEmpty() && end<=response.get(response.size()-1)[1]){
+        //         continue;
+        //     }
+        //     for(int j=i+1;j<n;j++){
+        //         if(intervals[j][0]<=end){
+        //             end = Math.max(end, intervals[j][1]);
+        //         }else{
+        //             break;
+        //         }
+        //     }
+        //     response.add(new int[]{start, end});
+        // }
+        // return response.toArray(new int[response.size()][]);
         
     }
 }
