514a7b97f846946c2d4360274484a5374ba5fa22
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-05-29 13:34:32 -0400

    Time: 27 ms (88.48%) | Memory: 52.5 MB (23.53%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,26 +1,64 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        //Brute force improving to O(n^2) skipping the 3rd loop using hashmap for finding the -(sum[i]+sum[j])
+        //Optimal approach 2-pointer, removing sorting using set and thus optimising space 
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        //i, j, k ->
+        //i has to be moved except when the first element until not same
+        //condition for when j>=k
+        //if sum>0 decrease k
+        //if sum<0 increase j
+        //if sum==0 decrease k + increase j until new elements 
         int n=nums.length;
-        Set<List<Integer>> set = new HashSet<>();
         for(int i=0;i<n;i++){
-            HashMap<Integer, Integer> map = new HashMap<>();
-            for(int j=i+1;j<n;j++){
-                int third = -(nums[i]+nums[j]);
-                if(map.containsKey(third)){
+            //i condition
+            if(i>0 && nums[i]==nums[i-1])continue;
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum>0){
+                   k--;
+                }
+                else if(sum<0){
+                    j++;
+                }
+                else{
                     List<Integer> temp = new ArrayList<>();
                     temp.add(nums[i]);
                     temp.add(nums[j]);
-                    temp.add(third);
-                    Collections.sort(temp);
-                    set.add(temp);
+                    temp.add(nums[k]);
+                    result.add(temp);
+                    j++;
+                    while(j<k && nums[j]==nums[j-1])j++;
+                    k--;
+                    while(j<k && nums[k]==nums[k+1])k--;
                 }
-                map.put(nums[j],0);
+                
             }
         }
-        List<List<Integer>> result = new ArrayList<>();
-        result.addAll(set);
-        return result;
+        return result;        
+        // //Brute force improving to O(n^2) skipping the 3rd loop using hashmap for finding the -(sum[i]+sum[j])
+        // int n=nums.length;
+        // Set<List<Integer>> set = new HashSet<>();
+        // for(int i=0;i<n;i++){
+        //     HashMap<Integer, Integer> map = new HashMap<>();
+        //     for(int j=i+1;j<n;j++){
+        //         int third = -(nums[i]+nums[j]);
+        //         if(map.containsKey(third)){
+        //             List<Integer> temp = new ArrayList<>();
+        //             temp.add(nums[i]);
+        //             temp.add(nums[j]);
+        //             temp.add(third);
+        //             Collections.sort(temp);
+        //             set.add(temp);
+        //         }
+        //         map.put(nums[j],0);
+        //     }
+        // }
+        // List<List<Integer>> result = new ArrayList<>();
+        // result.addAll(set);
+        // return result;
 
         // //Brute Force - 3 elements - O(n^3)
         // int n = nums.length;
