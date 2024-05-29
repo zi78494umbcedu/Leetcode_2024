8428e0405fde0091ff48bf4bd11a6873a26e7463
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-05-28 20:04:25 -0400

    Time: 2 ms (99.33%) | Memory: 47.9 MB (45.76%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,19 +1,62 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        //hasing O(nlogn)
+        //optimal approach Moore's Voting Algorithm with (element, count). Will the same work? 2 majority elements >n/3. (element1, count1), (element2, count2)
+        List<Integer> answer = new ArrayList<>(); 
+        int element1 = Integer.MIN_VALUE;
+        int element2=Integer.MIN_VALUE;
+        int count2=0;
+        int count1=0;
         int n=nums.length;
-        int minReq = (n/3)+1;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        List<Integer> result = new ArrayList<>(); 
         for(int i=0;i<n;i++){
-            int freq = map.getOrDefault(nums[i], 0); 
-            map.put(nums[i], freq+1);
-            if(map.get(nums[i])==minReq){
-                result.add(nums[i]);
-            }           
-            if(result.size()==2)break;
+            if(count1==0 && nums[i]!=element2){
+                count1=1;
+                element1=nums[i];
+            }else if(count2==0 && nums[i]!=element1){
+                count2=1;
+                element2=nums[i];
+            }else if(element1==nums[i]){
+                count1++;
+            }else if(element2==nums[i]){
+                count2++;
+            }else{
+                count1--;
+                count2--;
+            }
         }
-        return result;
+        int cntElement1=0;
+        int cntElement2=0;
+        for(int el:nums){
+            if(el==element1){
+                cntElement1++;
+            }
+            if(el==element2){
+                cntElement2++;
+            }
+        }
+        if(cntElement1>(n/3)){
+            answer.add(element1);
+        }
+    
+        if(cntElement2>(n/3)){
+            answer.add(element2);
+        }
+        return answer;
+        
+
+        // //hasing O(nlogn)
+        // int n=nums.length;
+        // int minReq = (n/3)+1;
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // List<Integer> result = new ArrayList<>(); 
+        // for(int i=0;i<n;i++){
+        //     int freq = map.getOrDefault(nums[i], 0); 
+        //     map.put(nums[i], freq+1);
+        //     if(map.get(nums[i])==minReq){
+        //         result.add(nums[i]);
+        //     }           
+        //     if(result.size()==2)break;
+        // }
+        // return result;
         //brute force 2 using 
         // List<Integer> res = new ArrayList<>();
         // int n=nums.length;
