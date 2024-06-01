72923298493b1bc69ed2f3d4c41b1e77a53d54b8
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-05-31 21:21:38 -0400

    Time: 0 ms (100.00%) | Memory: 41.6 MB (19.74%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -15,24 +15,51 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        //iterative method 2 stacks like Hanoi Tower
-        Stack<TreeNode> stack1 = new Stack<TreeNode>();
-        Stack<TreeNode> stack2 = new Stack<TreeNode>();
+        //iterative using 1 stack
         List<Integer> postOrderResult = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
         if(root==null)return postOrderResult;
-        stack1.add(root);
-        while(!stack1.isEmpty()){
-            TreeNode temp =stack1.pop();
-            stack2.add(temp);
-            if(temp.left!=null)stack1.add(temp.left);
-            if(temp.right!=null)stack1.add(temp.right);
-        }
-        while(!stack2.isEmpty()){
-            postOrderResult.add(stack2.pop().val);
+        TreeNode curr = root;
+        while(curr!=null || !stack.isEmpty()){
+            if(curr!=null){
+                stack.add(curr);
+                curr=curr.left;
+            }
+            else{
+                TreeNode temp = stack.peek().right;
+                if(temp==null){
+                    temp = stack.pop();
+                    postOrderResult.add(temp.val);
+                    while(!stack.isEmpty() && temp==stack.peek().right){
+                        temp = stack.pop();
+                        postOrderResult.add(temp.val);
+                    }
+                }
+                else{
+                    curr=temp;
+                }
+            }
         }
         return postOrderResult;
 
-        //return postOrder(root, new ArrayList<>());
+        // //iterative method 2 stacks like Hanoi Tower
+        // Stack<TreeNode> stack1 = new Stack<TreeNode>();
+        // Stack<TreeNode> stack2 = new Stack<TreeNode>();
+        // List<Integer> postOrderResult = new ArrayList<>();
+        // if(root==null)return postOrderResult;
+        // stack1.add(root);
+        // while(!stack1.isEmpty()){
+        //     TreeNode temp =stack1.pop();
+        //     stack2.add(temp);
+        //     if(temp.left!=null)stack1.add(temp.left);
+        //     if(temp.right!=null)stack1.add(temp.right);
+        // }
+        // while(!stack2.isEmpty()){
+        //     postOrderResult.add(stack2.pop().val);
+        // }
+        // return postOrderResult;
+
+        // //return postOrder(root, new ArrayList<>());
     }
     static List<Integer> postOrder(TreeNode root, List<Integer> traversal){
         if(root==null){
