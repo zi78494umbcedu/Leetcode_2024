512168249c36fae221c2b9a5f4a15c5c7b092fae
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-05-31 21:08:54 -0400

    Time: 0 ms (100.00%) | Memory: 41.5 MB (52.23%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -15,7 +15,24 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        return postOrder(root, new ArrayList<>());
+        //iterative method 2 stacks like Hanoi Tower
+        Stack<TreeNode> stack1 = new Stack<TreeNode>();
+        Stack<TreeNode> stack2 = new Stack<TreeNode>();
+        List<Integer> postOrderResult = new ArrayList<>();
+        if(root==null)return postOrderResult;
+        stack1.add(root);
+        while(!stack1.isEmpty()){
+            TreeNode temp =stack1.pop();
+            stack2.add(temp);
+            if(temp.left!=null)stack1.add(temp.left);
+            if(temp.right!=null)stack1.add(temp.right);
+        }
+        while(!stack2.isEmpty()){
+            postOrderResult.add(stack2.pop().val);
+        }
+        return postOrderResult;
+
+        //return postOrder(root, new ArrayList<>());
     }
     static List<Integer> postOrder(TreeNode root, List<Integer> traversal){
         if(root==null){
