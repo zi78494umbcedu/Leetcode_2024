56e9907432d2b1142bafa27715b80c6d6fc37325
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-06-02 02:22:14 -0400

    Update Solution.java

--- Children Sum Property/Solution.java
+++ Children Sum Property/Solution.java
@@ -31,6 +31,44 @@
 
  *****************************************************************/
 public class Solution {
+
+    static void makingValidChildrenSumProperty(Node root){
+        if(root==null){
+            return;
+        }
+        int child=0;
+        if(root.left!=null){
+            child+=root.left.data;
+        }
+        if(root.right!=null){
+            child+=root.right.data;
+        }
+        //if child value is greater than the parent.,parent =left+right
+        if(child>=root.data){
+            root.data = child;
+        }else{
+            //if child<parent then assign children the parent value
+            if(root.left!=null){
+                root.left.data = root.data;
+            }
+            if(root.right!=null){   
+                root.right.data = root.data;
+            }   
+        }
+        //traversals
+        makingValidChildrenSumProperty(root.left);
+        makingValidChildrenSumProperty(root.right);
+        //adding values from bottom up after replacing the Max(parent, children_sum);
+        int total = 0;
+        if(root.left!=null){
+                total+=root.left.data;
+        }
+         if(root.right!=null){
+                total+=root.right.data;
+        }
+        //checking if it is not a leaf node, only then we start to rise up
+        if(root.left!=null || root.right!=null)root.data=total;
+    }
     public static boolean isParentSum(Node root) {
        if(root==null)return true;
         if(root.left==null && root.right==null)return true;
