4ff8dc3c97108a1bc85bbd5bd2712b85079f5b32
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-06-02 21:48:09 -0400

    Create Solution.java

---
+++ Binary Search Tree/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public static boolean isValidBST(int []order){
+        for(int i=0;i<order.length-1;i++){
+            if(order[i]>=order[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
