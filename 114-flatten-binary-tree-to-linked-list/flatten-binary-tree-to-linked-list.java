478f2e28337e65725dd07d2fd4df761b6b59cd92
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-06-02 20:45:04 -0400

    Time: 0 ms (100.00%) | Memory: 42.2 MB (22.01%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -16,22 +16,39 @@
 class Solution {
     static TreeNode prev=null;
     public void flatten(TreeNode root) {
-        if(root==null)return;
-        Stack<TreeNode> st = new Stack<TreeNode>();
-        st.add(root);
-        while(!st.isEmpty()){
-            TreeNode curr = st.pop();
-            if(curr.right!=null){
-                st.add(curr.right);
-            }
+        //improving space Morris threading traversal
+        TreeNode curr= root;
+        while(curr!=null){
             if(curr.left!=null){
-                st.add(curr.left);
+                //forming thread with right-most to the curr
+                TreeNode prev =curr.left;
+                while(prev.right!=null){
+                    prev =prev.right;
+                }
+                prev.right =curr.right;
+                curr.right = curr.left;
+                curr.left= null;
             }
-            if(!st.isEmpty()){
-                curr.right = st.peek();
-            }
-            curr.left=null;
+            curr= curr.right;
         }
+        
+        //avoiding recursion
+        // if(root==null)return;
+        // Stack<TreeNode> st = new Stack<TreeNode>();
+        // st.add(root);
+        // while(!st.isEmpty()){
+        //     TreeNode curr = st.pop();
+        //     if(curr.right!=null){
+        //         st.add(curr.right);
+        //     }
+        //     if(curr.left!=null){
+        //         st.add(curr.left);
+        //     }
+        //     if(!st.isEmpty()){
+        //         curr.right = st.peek();
+        //     }
+        //     curr.left=null;
+        // }
         // if(root==null){
         //     return;
         // }
