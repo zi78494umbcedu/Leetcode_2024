5a722090163a498fd7049c8563bb49c8fdfdb099
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-06-06 16:12:44 -0400

    Time: 5 ms (45.91%) | Memory: 44.8 MB (45.17%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
@@ -1,38 +1,39 @@
 class Solution {
+
+    static void bfs(List<List<Integer>> adj, int node, boolean[] visited, int cnt){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(node);
+        visited[node]=true;
+        while(!q.isEmpty()){
+            int temp = q.poll();
+            for(int neighbour:adj.get(temp)){
+                if(!visited[neighbour]){
+                    visited[neighbour]=true;
+                    q.add(neighbour);
+                }
+            }
+        }
+    }
     public int countComponents(int n, int[][] edges) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+        //making the adjacency matrix in ArrayList<ArrayList<Integer>> for ease
+        List<List<Integer>> adj = new ArrayList<>();
         for(int i=0;i<n;i++){
-            adj.add(new ArrayList<Integer>());
+            adj.add(new ArrayList<>());
         }
-        for(int i=0;i<edges.length;i++){
-            int src = edges[i][0];
-            int dest = edges[i][1];
-            adj.get(src).add(dest);
-            adj.get(dest).add(src);
+        int e=edges.length;
+        for(int i=0;i<e;i++){
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
         }
+        //traversal to determine cnt everytime there is a hop
         boolean[] visited = new boolean[n];
-        int connt=0;
+        int cnt=0;
         for(int i=0;i<n;i++){
             if(!visited[i]){
-                connt++;
-                visited[i]=true;
-                markAllConnected(visited, i, adj);
+                cnt++;
+                bfs(adj, i, visited, cnt);
             }
         }
-        return connt;
+        return cnt;
     }
-    public static void markAllConnected(boolean[] visited, int i, ArrayList<ArrayList<Integer>> adj){
-       Queue<Integer> q = new LinkedList<>();
-       q.add(i);
-       while(!q.isEmpty()){
-        int current = q.poll();
-        for(int neighbour: adj.get(current)){
-            if(!visited[neighbour]){
-                visited[neighbour] = true;
-                q.add(neighbour);
-            }
-        }
-       }
-    }
-    
 }
