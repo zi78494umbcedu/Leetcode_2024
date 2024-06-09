497614f856400bd05f34a2fc3b85ac764d07d0c8
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-06-09 15:32:58 -0400

    Time: 25 ms (38.63%) | Memory: 55 MB (88.80%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -19,25 +19,64 @@ class Solution {
     }
     public List<Integer> eventualSafeNodes(int[][] graph) {
         //all the paths should lead to terminal nodes
-        //check for cycle
-        //any node part of cycle
-        //any node leading to a cycle 
-        //for readability better to have 2 arrays and not counters
+        //kahn's algorithm but outdegree instead owing to terminal nodes - breadth first traversal
+        //Thought: reverse edges to apply same algorithm of indegree 
+        //adjacency matrix in reverse + //indegree
+        List<List<Integer>> reverseAdj = new ArrayList<>();
         int n = graph.length;
-        boolean[] visited = new boolean[n];
-        boolean[] pathVisited = new boolean[n];
-        boolean[] checksSafe = new boolean[n];
         for(int i=0;i<n;i++){
-            if(!visited[i]){
-                dfs(graph, visited, pathVisited, checksSafe, i);
+            reverseAdj.add(new ArrayList<>());
+        }
+        int[] indegree = new int[n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<graph[i].length;j++){
+                reverseAdj.get(graph[i][j]).add(i);
+                indegree[i]++;
             }
         }
-        List<Integer> safeStates = new ArrayList<>();
+        //queue
+        Queue<Integer> q = new LinkedList<>();
         for(int i=0;i<n;i++){
-            if(checksSafe[i]){
-                safeStates.add(i);
+            if(indegree[i]==0){
+                q.add(i);
             }
         }
-        return safeStates;
+        //breadth first traversal
+        List<Integer> safeNodes = new ArrayList<>();
+        while(!q.isEmpty()){
+            int temp = q.poll();
+            safeNodes.add(temp);
+            for(int neighbour:reverseAdj.get(temp)){
+                indegree[neighbour]--;
+                if(indegree[neighbour]==0){
+                    q.add(neighbour);
+                }
+            }
+        }
+        Collections.sort(safeNodes);
+        return safeNodes;
+
+
+
+        // //check for cycle
+        // //any node part of cycle
+        // //any node leading to a cycle 
+        // //for readability better to have 2 arrays and not counters
+        // int n = graph.length;
+        // boolean[] visited = new boolean[n];
+        // boolean[] pathVisited = new boolean[n];
+        // boolean[] checksSafe = new boolean[n];
+        // for(int i=0;i<n;i++){
+        //     if(!visited[i]){
+        //         dfs(graph, visited, pathVisited, checksSafe, i);
+        //     }
+        // }
+        // List<Integer> safeStates = new ArrayList<>();
+        // for(int i=0;i<n;i++){
+        //     if(checksSafe[i]){
+        //         safeStates.add(i);
+        //     }
+        // }
+        // return safeStates;
     }
 }
