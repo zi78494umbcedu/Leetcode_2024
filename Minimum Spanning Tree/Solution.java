428d32dd7e7c4b693edb80d41b8b12380a8b1f4e
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-06-11 18:46:46 -0400

    Update Solution.java

--- Minimum Spanning Tree/Solution.java
+++ Minimum Spanning Tree/Solution.java
@@ -1,54 +1,63 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.List;
+import java.util.Comparator;
+
 
 class Pair{
 	int first;
 	int second;
 	public Pair(int first, int second){
-		this.first=first;
-		this.second=second;
+		this.first = first;
+		this.second= second;
 	}
 }
 public class Solution {
 
 	public static int minimumSpanningTree(ArrayList<ArrayList<Integer>> edges, int n) {
-		//Minimum Spanning Tree
-		//1.N nodes
-		//2.N-1 edges
-		//3.all nodes are reachable from each other
-		//Prim's Algorithm - Greedy, Priority Queue
-
-		//adjacency list
+		//Prim's algorithm - undirected graph 
+		//minimum spanning tree
+		/**
+		tree  - n nodes and n-1 edges and all nodes can be reached
+		find the minimum distanced edges tree
+		Use = Priority queues on edge weight/distance[node]
+		Use = visited node - update visited only when added to the MST data structure, 
+		add only when taking out of the PQ
+		keep adding neighbours for PQ to filter the minimum weighted edge
+		*/ 
+		//adjacency matrix
 		List<List<Pair>> adj = new ArrayList<>();
+		//zero based graph
 		for(int i=0;i<n;i++){
 			adj.add(new ArrayList<>());
 		}
 		for(int i=0;i<edges.size();i++){
-			//undirected
-			//weight, node
+			//undirected graph
 			adj.get(edges.get(i).get(0)).add(new Pair(edges.get(i).get(2), edges.get(i).get(1)));
 			adj.get(edges.get(i).get(1)).add(new Pair(edges.get(i).get(2), edges.get(i).get(0)));
 		}
-		PriorityQueue<Pair> pq = new PriorityQueue<>(Comparator.comparingInt(t->t.first));
-		boolean[] visited = new boolean[n];
-		int mstSum=0;
 		//weight, node
-		pq.add(new Pair(0,0));
+		PriorityQueue<Pair> pq = new PriorityQueue<>(Comparator.comparingInt(t->t.first));
+		//visited data structure
+		boolean[] visited = new boolean[n];
+		//mst data structure
+		int minWeight=0;
+		//initialising
+		pq.add(new Pair(0, 0));
 		while(!pq.isEmpty()){
 			Pair temp = pq.poll();
-			int weight= temp.first;
-			int node = temp.second;
-			if(visited[node])continue;
-			visited[node]=true;
-			mstSum+=weight;
-			
-			for(Pair neighbour:adj.get(node)){
-				int neighbourNode= neighbour.second;
-				int neighbourWeight = neighbour.first;
-				if(!visited[neighbourNode]){
-					pq.add(new Pair(neighbourWeight, neighbourNode));
-				}
+			int tempWeight= temp.first;
+			int tempNode = temp.second;
+			if(visited[tempNode])continue;
+			visited[tempNode]=true;
+			minWeight+=tempWeight;
+			for(Pair neighbour: adj.get(tempNode)){
+				int weightNeighbour=neighbour.first;
+				int nodeNeighbour=neighbour.second;
+				pq.add(new Pair(weightNeighbour, nodeNeighbour));
 			}
-		}
-		return mstSum;
+		} 
+		return minWeight;
 	}
 }
