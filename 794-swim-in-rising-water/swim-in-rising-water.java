709790778fad1c17e7f273e1001daed77be2d762
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-06-13 01:05:40 -0400

    Time: 23 ms (18.95%) | Memory: 44.1 MB (76.21%) - LeetSync

---
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -0,0 +1,118 @@
+class DisjointSet{
+    List<Integer> parent =new ArrayList<>();
+     List<Integer> rank =new ArrayList<>();
+      List<Integer> size =new ArrayList<>();
+
+      public DisjointSet(int n){
+        for(int i=0;i<n;i++){
+            parent.add(i);
+            rank.add(0);
+            size.add(1);
+        }
+      }
+      public int getParent(int node){
+        if(node==parent.get(node)){
+            return node;
+        }
+        int interimParent = getParent(parent.get(node));
+        parent.set(node, interimParent);
+        return parent.get(node);
+      }
+      public void unionBySize(int u, int v){
+        int parentU = getParent(u);
+        int parentV = getParent(v);
+        if(parentU==parentV)return ;
+        int sizeU = size.get(parentU);
+        int sizeV = size.get(parentV);
+        if(sizeU>sizeV){
+            parent.set(parentV,parentU);
+            size.set(parentU, sizeU+sizeV);
+        }else{
+             parent.set(parentU,parentV);
+             size.set(parentV, sizeU+sizeV);
+        }
+      }
+      public void unionByRank(int u, int v){
+        int parentU = getParent(u);
+        int parentV = getParent(v);
+        if(parentU==parentV)return ;
+        int rankU = rank.get(parentU);
+        int rankV= rank.get(parentV);
+        if(rankU>rankV){
+            parent.set(parentV,parentU);
+        }else if(rankU<rankV){
+             parent.set(parentU,parentV);
+        }else{
+            parent.set(parentU,parentV);
+            rank.set(parentV, rank.get(parentV)+1);
+        }
+      }
+}
+
+class Solution {
+    public int swimInWater(int[][] grid) {
+        //krushal's algorithm 
+        //sorting in time ascending
+        //making a minimum spanning tree types union the positions
+        int n=grid.length;
+        DisjointSet ds = new DisjointSet(n*n);
+        List<Integer> positions = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                positions.add(i*n+j);
+            }
+        }
+        Collections.sort(positions, (a,b)->grid[a/n][a%n]-grid[b/n][b%n]);
+        int[] rows = {-1, 0, 1, 0};
+        int[] cols = {0, -1, 0, 1};
+        for(int position: positions){
+            int row=position/n;
+            int col=position%n;
+            for(int i=0;i<4;i++){
+                int ru=row+rows[i];
+                int cu=col+cols[i];
+                if(ru>=0 && ru<n && cu>=0 && cu<n && grid[ru][cu]<grid[row][col]){
+                    ds.unionBySize(position, ru*n+cu);
+                }
+            }
+            if(ds.getParent(0)==ds.getParent(n*n-1)){
+                return grid[row][col];
+            }
+        }
+        return -1;
+    }
+}
+    
+        // HashMap<Integer, int[]> map = new HashMap<>();
+        // int n = grid.length;
+
+        // if (n == 1)
+        //     return 0;
+
+        // DisjointSet ds = new DisjointSet(n * n);
+
+        // for (int i = 0; i < n; i++)
+        //     for (int j = 0; j < n; j++)
+        //         map.put(grid[i][j], new int[]{i, j});
+
+        // int time = 0;
+
+        // while (ds.findParent(0) != ds.findParent(n * n - 1)) {
+        //     int[] node = map.get(time);
+        //     int x = node[0];
+        //     int y = node[1];
+        //     int[] dx = {-1, 0, 1, 0};
+        //     int[] dy = {0, -1, 0, 1};
+
+        //     for (int i = 0; i < 4; i++) {
+        //         int nrow = x + dx[i];
+        //         int ncol = y + dy[i];
+
+        //         if (nrow >= 0 && ncol >= 0 && nrow < n && ncol < n && grid[nrow][ncol] <= time)
+        //             ds.unionBySize(nrow * n + ncol, x * n + y);
+        //     }
+
+        //     time++;
+        // }
+
+        // return time - 1;
