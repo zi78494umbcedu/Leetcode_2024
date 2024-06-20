c6a8e516aa77fd583563dbd6f75844da69d9e27d
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-06-19 23:20:58 -0400

    Time: 0 ms (100.00%) | Memory: 42.4 MB (92.22%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -4,18 +4,18 @@ class Solution {
         int start =0;
         int end = len-1;
         
-        int ans = len;
+        //int ans = len;
         while(start<=end){
             int mid = (start+end)/2;
             if(nums[mid]==target)return mid;
             else if(nums[mid]>=target){
                 end = mid-1;
-                ans = mid;
+                //ans = mid;
 
             }else{
                 start=mid+1;
             }
         }
-        return ans;
+        return start;
     }
 }
