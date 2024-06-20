58657e3cef337f32ef93539945686582aa7841cc
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-06-19 22:55:53 -0400

    Time: 311 ms (95.17%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,21 +1,16 @@
-SELECT visited_on, 
-    (
-        SELECT SUM(amount) 
-        FROM Customer 
-        WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) 
-        AND c.visited_on 
-    ) AS amount, ROUND(
-        (
-           SELECT SUM(amount)/7 
-            FROM Customer 
-            WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) 
-            AND c.visited_on 
-        ), 2
-    ) AS average_amount
-FROM Customer c 
-WHERE visited_on >= (
-    SELECT DATE_ADD(MIN(visited_on), INTERVAL 6 DAY) 
-    FROM Customer 
-) 
-GROUP BY visited_on 
-ORDER BY visited_on;
+select visited_on, 
+    (select sum(amount) 
+    from customer
+    where visited_on between date_sub(c.visited_on, interval 6 day)
+    and c.visited_on
+    ) as amount,
+    round((select sum(amount)/7 
+    from customer 
+    where visited_on between date_sub(c.visited_on, interval 6 day) 
+    and c.visited_on
+    ), 2) as average_amount
+from customer c
+where visited_on>=
+    (select date_add(min(visited_on), interval 6 day) from customer)
+group by visited_on
+order by visited_on
