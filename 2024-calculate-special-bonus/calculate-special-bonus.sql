7ce8a33cdefa53c83c4127a2c18b0fd4ca422db9
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-08-01 12:32:12 -0400

    Time: 683 ms (23.11%) | Memory: 0B (100.00%) - LeetSync

--- 2024-calculate-special-bonus/calculate-special-bonus.sql
+++ 2024-calculate-special-bonus/calculate-special-bonus.sql
@@ -1,23 +1,6 @@
 # Write your MySQL query statement below
 
-
--- entries with not M and odd employee_id
 select employee_id, 
-case 
-when employee_id%2<>0 and name not like 'M%' then salary 
-else 0
-end 
-as bonus 
+if(MOD(employee_id, 2)=1 and name not like 'M%',salary, 0) as bonus
 from employees
 order by employee_id
-
--- select employee_id, 
--- coalesce(
---     (select salary 
---     from Employees
---     where name not like 'M%'
---     and employee_id%2!=0
---     and e.employee_id=employee_id
---     ), 0)  as bonus 
--- from employees e
--- order by employee_id
