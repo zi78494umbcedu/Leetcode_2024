7dff35b8d11bd3dd3597a875053166b2a797327f
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-08-02 12:36:39 -0400

    Time: 709 ms (84.14%) | Memory: 0B (100.00%) - LeetSync

--- 1481-students-with-invalid-departments/students-with-invalid-departments.sql
+++ 1481-students-with-invalid-departments/students-with-invalid-departments.sql
@@ -1,6 +1,4 @@
 # Write your MySQL query statement below
 
-select id, name from students where id not in (
-select s.id from students s
-join departments d
-on d.id=s.department_id)
+select id, name from students where department_id not in (
+select id from departments)
