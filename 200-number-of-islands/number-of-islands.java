95d75e5b217c9f4c672a924d85bfaa6fda5a2de9
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-08-16 07:36:55 -0400

    Time: 9 ms (10.65%) | Memory: 52.7 MB (5.81%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,59 +1,47 @@
-class Solution {
-
-    public static void dfs(int i, int j, boolean[] visited, char[][] grid, int m , int n){
-        visited[i*n+j]=true;
-        /**
-        right - 0, +1
-        +1, +1
-        down +1, 0
-        +1, -1
-        left 0, -1
-        -1, -1
-        up -1, 0
-        -1, +1
-         */
-        int[] dx= {0, +1, 0, -1};
-        int[] dy= { +1, 0, -1,  0};
-        for(int p=0;p<4;p++){
-            int du = i+dx[p];
-            int dv = j+dy[p];
-            if(du<m && du>=0 && dv<n && dv>=0 && grid[du][dv]=='1' && !visited[du*n+dv]){
-                dfs(du, dv, visited,grid,  m, n);
-            }
-        }
-
+class Pair{
+    int x;
+    int y;
+    public Pair(int x, int y){
+        this.x= x;
+        this.y=y;
     }
-
+}
+class Solution {
     public int numIslands(char[][] grid) {
         /**
-        traverse through the matrix to find the first 1
-        do traversal marking visited and counting numIslands
-        basic dfs that can travel in all 8 directions 
-        visited array of size m*n
-         */
-        int m = grid.length;
+        visited array of m*n 
+        bfs using a queue with 4 directions
+        count of islands on starting nodes of islands
+        */
+        int m=grid.length;
         int n = grid[0].length;
         boolean[] visited = new boolean[m*n+1];
-        int numIslands=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]=='1' && !visited[i*n+j]){
-                    numIslands++;
-                    dfs(i, j, visited, grid, m, n);
+        int islands =0;
+        Queue<Pair> q = new LinkedList<>();
+        int[] du = {0, +1, 0, -1};
+        int[] dy = {+1, 0, -1, 0};
+        for(int t=0;t<m;t++){
+            for(int o=0;o<n;o++){
+                if(grid[t][o]=='1' && !visited[t*n+o]){
+                    visited[t*n+o]=true;
+                    q.add(new Pair(t, o));
+                    islands++;
+                    while(!q.isEmpty()){
+                        Pair pair = q.poll();
+                        int xu = pair.x;
+                        int yu = pair.y;
+                        for(int i=0;i<4;i++){
+                            int xv = xu+du[i];
+                            int yv = yu+dy[i];
+                            if(xv>=0 && xv<m && yv>=0 && yv<n && grid[xv][yv]=='1' && !visited[xv*n+yv]){
+                                q.add(new Pair(xv, yv));
+                                visited[xv*n+yv]=true;
+                            }
+                        }
+                    }
                 }
             }
         }
-        for(int i=0;i<m*n;i++){
-            System.out.println(visited[i]);
-        }
-        return numIslands;
-
+        return islands;
     }
 }
-/// 4,5
-/// 0, 1, 2, 3, 4
-/// 5, 6, 7, 8, 9
-
-/// 0,0, 0,1 0,2 0,3 0,4
-/// 1,0, 1,1, 1,2 1,3 1,4
-
