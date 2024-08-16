36e587d00d539b122cb19d07108cff86b2038544
Ashwin Gupta <125627136+zi78494umbcedu@users.noreply.github.com>
2024-08-15 21:06:01 -0400

    Time: 1 ms (91.37%) | Memory: 44.7 MB (98.11%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,36 +1,22 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        //convert matrix into adjacency list
-         int n = isConnected.length;
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<=n;i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1){
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
-                }
-            }
-        }
-        //basic DFS, once not found increase counter
-        boolean[] visited = new boolean[n+1];
-        int provinces =0;
-        //recursion for DFS
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        int count=0;
         for(int i=0;i<n;i++){
             if(!visited[i]){
-                provinces++;
-                dfs(i, adj, visited);
+                count++;
+                dfs(isConnected, i, visited);
             }
         }
-        return provinces;
+        return count;
     }
-    static void dfs(int i, List<List<Integer>> adj, boolean[] visited){
-        visited[i]=true;
-        for(int neighbour:adj.get(i)){
-            if(!visited[neighbour]){
-                dfs(neighbour, adj, visited);
+
+    public static void dfs(int[][] isConnected, int curr, boolean[] visited){
+        visited[curr]=true;
+        for(int i=0;i<isConnected[curr].length;i++){
+            if(isConnected[curr][i]==1 && !visited[i]){
+                dfs(isConnected, i, visited);
             }
         }
     }
